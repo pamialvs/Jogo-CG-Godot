d422d8cf43bca31857292d2ce23b10251c1f3918
Pâmela Fernandes <pamelawallescaf@gmail.com>
2025-11-30 19:19:32 -0300

    Revert "Merge branch 'main' of https://github.com/pamialvs/Jogo-CG-Godot"
    This reverts commit 67a667e57e9bee3077f84a25b324912d9d72c293, reversing
    changes made to 2816eb2d437447da07d218f31ca3d1a0fbddec5c.

--- sabrina---o-mundo-sombrio/scripts/coin.gd
+++ sabrina---o-mundo-sombrio/scripts/coin.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-var potions := 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,10 +13,7 @@ func _process(delta: float):
 
 func _on_body_entered(body: Node2D):
 	$anim.play("collect")
-	#Evita a colisão dupla de poções
-	await $collision.call_deferred("queue_free")
-	Globals.potions += potions
-	print(Globals.potions)
+
 
 func _on_anim_animation_finished():
 	queue_free()
--- sabrina---o-mundo-sombrio/singletons/globals.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-var potions := 0
