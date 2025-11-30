aa32c4f22fd76a9447f1f7497066e1489bd33ed8
Vinicius Gomes <167696113+vinxciusg@users.noreply.github.com>
2025-11-30 14:38:04 -0300

    colocando HUD

--- sabrina---o-mundo-sombrio/scripts/coin.gd
+++ sabrina---o-mundo-sombrio/scripts/coin.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var potions := 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,7 +14,10 @@ func _process(delta: float):
 
 func _on_body_entered(body: Node2D):
 	$anim.play("collect")
-
+	#Evita a colisão dupla de poções
+	await $collision.call_deferred("queue_free")
+	Globals.potions += potions
+	print(Globals.potions)
 
 func _on_anim_animation_finished():
 	queue_free()
